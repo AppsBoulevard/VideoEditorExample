b77db386cc5812b9c04e608b2a346a033b0cf55b
Oscar J. Irun <oscarjiv91@gmail.com>
2016-12-10 13:14:35 -0300

    Initial Commit

---
+++ videoeditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  videoeditor
+//
+//  Created by Oscar J. Irun on 10/12/16.
+//  Copyright © 2016 appsboulevard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
